e20ccbf600fe2445a944b850c155516c1f925a19
alu0101056944@ull.edu.es <alu0101056944@ull.edu.es>
2023-10-18 21:48:38 +0100

    HSPI SPI setup in eeprom.

--- src/eeprom_main.cpp
+++ src/eeprom_main.cpp
@@ -20,22 +20,22 @@ MemoryEEPROM eeprom;
 
 uint8_t obtainedByte = 0x66; // dummy value
 
-SPIClass hspi(HSPI); // ESP32 apparently requires to declare which SPI instance to use
+#define HSPI_MISO   12
+#define HSPI_MOSI   13
+#define HSPI_SCLK   14
+
+bool enabled;
 
 void setup() {
   pinMode(CHIP_SELECT_EEPROM, OUTPUT);
-  hspi.begin();
+  SPIClass hspi(HSPI); // ESP32 apparently requires to declare which SPI instance to use
+  hspi.begin(HSPI_SCLK, HSPI_MISO, HSPI_MOSI, CHIP_SELECT_EEPROM);
   Serial.begin(9600);
   delay(1000);
   eeprom.enableWrite();
   delay(1);
-  bool enabled = eeprom.isWriteEnabled();
+  enabled = eeprom.isWriteEnabled();
   delay(1);
-  // if (enabled) {
-  //   Serial.println("Write is enabled");
-  // } else {
-  //   Serial.println("Write is disabled.");
-  // }
   const uint8_t kByteToWrite = 0x83;
   eeprom.writeByte(kByteToWrite, 22222); // arbitrary address
   //eeprom.waitUntilReady();
@@ -45,29 +45,34 @@ void setup() {
 
 void loop() {
   Serial.println("Obtained byte value from EEPROM read operation: ");
-  Serial.print(HSPI);
+  Serial.print(obtainedByte);
   Serial.println();
+  if (enabled) {
+    Serial.println("Write is enabled");
+  } else {
+    Serial.println("Write is disabled.");
+  }
   // Serial.print(obtainedByte);
   // Serial.println();
-  // int valueOfInput = digitalRead(13);
-  // Serial.print("Input pin: ");
-  // Serial.print(valueOfInput);
-  // Serial.println();
-  // int valueOfOutput = digitalRead(12);
-  // Serial.print("Output pin: ");
-  // Serial.print(valueOfOutput);
-  // Serial.println();
-  // int valueOfChipSelect = digitalRead(18);
-  // Serial.print("Chip select pin:valueOf ");
-  // Serial.print(valueOfChipSelect);
-  // Serial.println();
-  // int valueOfHold = digitalRead(19);
-  // Serial.print("Hold pin: ");
-  // Serial.print(valueOfHold);
-  // Serial.println();
-  // int valueOfWrite = digitalRead(20);
-  // Serial.print("Write protect pinvalueOf: ");
-  // Serial.print(valueOfWrite);
-  // Serial.println();
+  // // int valueOfInput = digitalRead(13);
+  // // Serial.print("Input pin: ");
+  // // Serial.print(valueOfInput);
+  // // Serial.println();
+  // // int valueOfOutput = digitalRead(12);
+  // // Serial.print("Output pin: ");
+  // // Serial.print(valueOfOutput);
+  // // Serial.println();
+  // // int valueOfChipSelect = digitalRead(18);
+  // // Serial.print("Chip select pin:valueOf ");
+  // // Serial.print(valueOfChipSelect);
+  // // Serial.println();
+  // // int valueOfHold = digitalRead(19);
+  // // Serial.print("Hold pin: ");
+  // // Serial.print(valueOfHold);
+  // // Serial.println();
+  // // int valueOfWrite = digitalRead(20);
+  // // Serial.print("Write protect pinvalueOf: ");
+  // // Serial.print(valueOfWrite);
+  // // Serial.println();
   delay(1000);
 }
